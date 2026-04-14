2e5a4eaba538c7f30417a3a1041a2c2d6f85ed8f
stasbuzenkov <buzenkov1980@bk.ru>
2026-04-14 08:01:48 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Radio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.radioPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.radioPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(135, 77);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(472, 31);
+            this.urlTextBox.TabIndex = 0;
+            this.urlTextBox.TextChanged += new System.EventHandler(this.urlTextBox_TextChanged);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(63, 282);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(181, 70);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "button1";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(558, 282);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(181, 70);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "button2";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // radioPlayer
+            // 
+            this.radioPlayer.Enabled = true;
+            this.radioPlayer.Location = new System.Drawing.Point(325, 383);
+            this.radioPlayer.Name = "radioPlayer";
+            this.radioPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("radioPlayer.OcxState")));
+            this.radioPlayer.Size = new System.Drawing.Size(75, 23);
+            this.radioPlayer.TabIndex = 3;
+            this.radioPlayer.Enter += new System.EventHandler(this.radioPlayer_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioPlayer);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.urlTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.radioPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button stopButton;
+        private AxWMPLib.AxWindowsMediaPlayer radioPlayer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Radio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(urlTextBox.Text))
+            {
+                radioPlayer.URL = urlTextBox.Text;
+                radioPlayer.Ctlcontrols.play();
+            }
+            else
+            {
+                MessageBox.Show("Введите URL радиостанции!");
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            radioPlayer.Ctlcontrols.stop();
+        }
+
+        private void urlTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioPlayer_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
